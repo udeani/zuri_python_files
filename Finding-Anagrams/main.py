ad61bfca677d1a27ded65615ad0e8d33e02d4510
henry <udeani.henry@gmail.com>
2022-05-28 08:58:21 +0100

    changing the output of finding_anagrams from print to return

--- Finding-Anagrams/main.py
+++ Finding-Anagrams/main.py
@@ -7,9 +7,9 @@
 def find_anagram(word, anagram):
     # [assignment] Add your code here
     if sorted(word) == sorted(anagram):
-        print (True)
+        return (True)
     else:
-    	print (False)
+    	return (False)
 
-find_anagram("below", "elbow")
-find_anagram("hello", "check")
+#print(find_anagram("below", "elbow"))
+#print(find_anagram("hello", "check"))
